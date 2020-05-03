3e260f165d748631cedc2fa0c65ad0c702e9050b
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-03 13:42:53 +0200

    les classes des formes Carre, Triangle, Cercle, Rectangle

---
+++ src/main/java/fr/uvsq/pglp_9_9/Cercle.java
@@ -0,0 +1,17 @@
+package fr.uvsq.pglp_9_9;
+
+public class Cercle extends Form{
+
+	@Override
+	public void afficher() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deplacer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
