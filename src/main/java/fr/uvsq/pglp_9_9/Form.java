ebdf5c07e92381d7d8daae7fd3118254126f9713
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-03 13:42:08 +0200

    la classe form

---
+++ src/main/java/fr/uvsq/pglp_9_9/Form.java
@@ -0,0 +1,10 @@
+package fr.uvsq.pglp_9_9;
+
+public abstract class Form {
+	// pour afficher les forme
+	public abstract void afficher();
+
+	// pour deplacer les formes
+	public abstract void deplacer();
+
+}
