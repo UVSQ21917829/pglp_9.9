c9b3357787a457510d1991b26abbff52d51db215
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-16 13:24:04 +0200

    modifier la lasse Form

--- src/main/java/fr/uvsq/pglp_9_9/Form.java
+++ src/main/java/fr/uvsq/pglp_9_9/Form.java
@@ -1,10 +1,24 @@
 package fr.uvsq.pglp_9_9;
 
 public abstract class Form {
+	private String nom;
+	public Form(String nom) {
+		this.nom=nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
 	// pour afficher les forme
 	public abstract void afficher();
 
 	// pour deplacer les formes
-	public abstract void deplacer();
+	public abstract void decaler(int x, int y);
+	public abstract void deplacer(int x, int y);
 
 }
