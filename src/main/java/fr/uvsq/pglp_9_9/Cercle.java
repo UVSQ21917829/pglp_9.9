067473db936118d4c8b4bcbb181a0d33936e4c6c
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-16 13:24:49 +0200

    implementer la classe cercle

--- src/main/java/fr/uvsq/pglp_9_9/Cercle.java
+++ src/main/java/fr/uvsq/pglp_9_9/Cercle.java
@@ -1,6 +1,19 @@
 package fr.uvsq.pglp_9_9;
 
 public class Cercle extends Form{
+    private Point2D centre;
+	public Cercle(String nom,Point2D centre) {
+		super(nom);
+		this.setCentre(centre);
+	}
+
+	public Point2D getCentre() {
+		return centre;
+	}
+
+	public void setCentre(Point2D centre) {
+		this.centre = centre;
+	}
 
 	@Override
 	public void afficher() {
@@ -9,9 +22,16 @@ public class Cercle extends Form{
 	}
 
 	@Override
-	public void deplacer() {
-		// TODO Auto-generated method stub
+	public void deplacer(int x, int y) {
+		centre.deplaccer(x, y);
 		
 	}
+	@Override
+	public void decaler(int x, int y) {
+		this.centre.decaler(x, y);
+		
+	}
+
+	
 
 }
