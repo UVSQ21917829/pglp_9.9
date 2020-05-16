66fbf4cf22b937891fd59624f699ba5f29429d0c
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-16 13:24:27 +0200

    implementer la class carre

--- src/main/java/fr/uvsq/pglp_9_9/Carre.java
+++ src/main/java/fr/uvsq/pglp_9_9/Carre.java
@@ -2,6 +2,9 @@ package fr.uvsq.pglp_9_9;
 
 public class Carre extends Form{
 
+	private Point2D centreGravite;
+	private int cote;
+	
 	@Override
 	public void afficher() {
 		// TODO Auto-generated method stub
@@ -9,9 +12,35 @@ public class Carre extends Form{
 	}
 
 	@Override
-	public void deplacer() {
-		// TODO Auto-generated method stub
+	public void deplacer(int x,int y) {
+		centreGravite.deplaccer(x, y);
 		
+	}
+	@Override
+	public void decaler(int x, int y) {
+		this.centreGravite.decaler(x, y);
+		
+	}
+	public Carre(String nom,Point2D centreGravite, int cote) {
+		super(nom);
+		this.centreGravite = centreGravite;
+		this.cote = cote;
+	}
+
+	public Point2D getCentreGravite() {
+		return centreGravite;
+	}
+
+	public void setCentreGravite(Point2D centreGravite) {
+		this.centreGravite = centreGravite;
+	}
+
+	public int getCote() {
+		return cote;
+	}
+
+	public void setCote(int cote) {
+		this.cote = cote;
 	}
 
 }
