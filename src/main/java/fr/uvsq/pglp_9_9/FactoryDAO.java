a78dac883cd8abf9fc00990aca08f56cf255e268
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-17 19:51:59 +0200

    factorydao

---
+++ src/main/java/fr/uvsq/pglp_9_9/FactoryDAO.java
@@ -0,0 +1,26 @@
+package fr.uvsq.pglp_9_9;
+
+public class FactoryDAO {
+
+	public DAO<Cercle> getCercleDAO() {
+		return new CercleDAO();
+
+	}
+
+	public DAO<Carre> getCarreDAO() {
+		return new CarreDAO();
+
+	}
+	public DAO<Rectangle> getRectangleDAO() {
+		return new RectangleDAO();
+
+	}
+	public DAO<Triangle> getTriangleDAO() {
+		return new TriangleDAO();
+
+	}
+	public DAO<Groupe> getGroupeDAO() {
+		return new GroupeDAO();
+
+	}
+}
