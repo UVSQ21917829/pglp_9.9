167e9b4561dcbdf60091d453c68ab317606bb918
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-05-18 10:39:01 +0200

    corriger

--- src/main/java/fr/uvsq/pglp_9_9/DeleteCommand.java
+++ src/main/java/fr/uvsq/pglp_9_9/DeleteCommand.java
@@ -2,65 +2,89 @@ package fr.uvsq.pglp_9_9;
 
 import java.io.IOException;
 
-public class DeleteCommand implements Command{
+public class DeleteCommand implements Command {
 
+	private String nom;
 	private Form forme;
+	private Groupe dessin;
 	private FactoryDAO dao;
-	public DeleteCommand(Form forme) {
+
+	public DeleteCommand(String npm, Groupe dessin) {
 		this.setForme(forme);
-		dao= new FactoryDAO();
+		this.dessin = dessin;
+		dao = new FactoryDAO();
 	}
+
 	public Form getForme() {
 		return forme;
 	}
+
 	public void setForme(Form forme) {
 		this.forme = forme;
 	}
-	
+
+	public boolean exist(Form forme2) {
+		for (Form form : dessin.getForms()) {
+			if (form.getNom().equals(forme2.getNom())) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	@Override
 	public void execute() {
-	
-		if (forme instanceof Carre) {
-			try {
-				dao.getCarreDAO().delete( forme.getNom());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-		} else if (forme instanceof Cercle) {
-			try {
-				dao.getCercleDAO().delete( forme.getNom());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 
-		} else if (forme instanceof Rectangle) {
-			try {
-				dao.getRectangleDAO().delete( forme.getNom());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		if (!this.exist(this.forme)) {
+
+			for (int i = 0; i < dessin.getForms().size(); i++) {
+				if (dessin.getForms().get(i).getNom().equals(nom)) {
+					dessin.getForms().remove(i);
+				}
 			}
-			
-		} else if (forme instanceof Triangle) {
-			try {
-				dao.getTriangleDAO().delete( forme.getNom());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			if (forme instanceof Carre) {
+				try {
+
+					dao.getCarreDAO().delete(nom);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else if (forme instanceof Cercle) {
+				try {
+					dao.getCercleDAO().delete(nom);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else if (forme instanceof Rectangle) {
+				try {
+					dao.getRectangleDAO().delete(nom);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else if (forme instanceof Triangle) {
+				try {
+					dao.getTriangleDAO().delete(nom);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else if (forme instanceof Groupe) {
+				try {
+					dao.getGroupeDAO().delete(nom);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			
-		} else if (forme instanceof Groupe) {
-			try {
-				dao.getGroupeDAO().delete( forme.getNom());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}		
-		
+		}
 	}
 
 }
